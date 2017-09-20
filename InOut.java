3021c9ab48a85961f9ee1d2f36eb113a06289f7e
zhanggen <zhanggenex@qq.com>
2017-09-20 17:56:04 +0800

    source code libs

---
+++ InOut.java
@@ -0,0 +1,43 @@
+
+
+
+
+public class InOut {
+    
+    
+    private int id, cinout;
+    private String inout;
+   
+
+    public InOut(int id, String inout, int cinout) {
+        this.id = id;
+        this.inout = inout;
+        this.cinout = cinout;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getInout() {
+        return inout;
+    }
+
+    public void setInout(String inout) {
+        this.inout = inout;
+    }
+
+    public int getCinout() {
+        return cinout;
+    }
+
+    public void setCinout(int cinout) {
+        this.cinout = cinout;
+    }
+    
+    
+}
