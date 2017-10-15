cd37dcd0dea329b376ae68fd6838c2889e958eec
zhanggenex <zhanggenex@qq.com>
2017-10-15 09:45:52 +0800

    Source code of our test case prioritization techniques

--- Prioritization.java
+++ Prioritization.java
@@ -1,4 +1,4 @@
-
+ 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-import java.util.HashSet;
+ import java.util.HashSet;
 import java.util.Set;
 
 public class Test {
